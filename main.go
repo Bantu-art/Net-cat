a2db2bb70b4a9f40d5a7c83314fb57d8fbc96799
bbantu <brianbaraza40@gmail.com.com>
2024-10-24 17:15:34 +0300

    Add main function to initialize server and handle connections - Retrieves port using netcat.GetPort() - Starts server using netcat.StartServer() - Accepts client connections in a loop and handles them with netcat.HandleConnection()

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"netcat/netcat"
+)
+
+func main() {
+	port := netcat.GetPort()
+	if port == "" {
+		return
+	}
+
+	listener, err := netcat.StartServer(port)
+	if err != nil {
+		fmt.Printf("Failed to start server: %v\n", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Printf("Listening on the port :%s\n", port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Failed to accept connection: %v\n", err)
+			continue
+		}
+		netcat.HandleConnection(conn)
+	}
+}
