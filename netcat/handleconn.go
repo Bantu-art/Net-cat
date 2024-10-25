600b5db016939400661143626ba415bd8deeaa3f
bbantu <brianbaraza40@gmail.com.com>
2024-10-25 17:58:37 +0300

    feat: add client handling with ASCII welcome message in TCP chat
    - Added  constant with an ASCII art and name prompt for new clients.
    - Introduced  struct to represent a connected client with a name and connection.
    -  registers the client and prints the client's name upon successful registration.
    - Improved error handling during client registration to handle failures gracefully.

--- netcat/handleconn.go
+++ netcat/handleconn.go
@@ -5,8 +5,39 @@ import (
 	"net"
 )
 
+const WelcomeMessage = `Welcome to TCP-Chat!
+         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ __| ".        |\dS"qML
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     '-'       '--'
+[ENTER YOUR NAME]: `
+
+type Client struct {
+	Conn net.Conn
+	Name string
+}
+
+// HandleConnection manages a single client connection
 func HandleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	fmt.Printf("New connection from: %s\n", conn.RemoteAddr().String())
+	client, err := RegisterClient(conn)
+	if err != nil {
+		fmt.Printf("Failed to register client: %v\n", err)
+		return
+	}
+
+	fmt.Printf("New client registered: %s\n", client.Name)
 }
