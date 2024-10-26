ec8f92a159018ee150f3d594e2d019a17900ebc3
bbantu <brianbaraza40@gmail.com.com>
2024-10-27 01:20:06 +0300

    refactor: change if statement to reflect the name and not the message

--- netcat/formatmsg.go
+++ netcat/formatmsg.go
@@ -8,7 +8,7 @@ import (
 func FormatMessage(name, message string) string {
 	timestamp := time.Now().Format("2006-01-02 15:04:05")
 
-	if message == "" {
+	if name == "" {
 		return fmt.Sprintf("[%s]%s", timestamp, message)
 	}
 
