d3844e22762ddefad8c35479655efd1cad62e64f
jamos <amosjoel91@gmail.com>
2024-10-29 13:14:38 +0300

    fix: remove double printed message

--- netcat/broadcast.go
+++ netcat/broadcast.go
@@ -1,12 +1,20 @@
 package netcat
 
-func Broadcast(name, message string, history *History) {
-	formattedMsg := FormatMessage(name, message)
-	history.Save(formattedMsg)
+import "fmt"
+
+func Broadcast(sender *Client, message string, history *History) {
+	formattedMsg := FormatMessage(sender.Name, message)
+	history.Save(formattedMsg + "\n")
 	mutex.Lock()
 	defer mutex.Unlock()
 
+	fmt.Fprint(sender.Conn, "\033[2K\r")
+	fmt.Fprintln(sender.Conn, formattedMsg)
+
 	for client := range clients {
+		if client == sender {
+			continue
+		}
 		client.Conn.Write([]byte(formattedMsg + "\n"))
 	}
 }
