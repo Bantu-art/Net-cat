9d59112c2173598a81800d8679d6fc907aa5c5f2
oduortoni <oduortoni@gmail.com>
2024-11-05 10:46:53 +0300

    feat: add tests for trimSpace

--- netcat/utils_test.go
+++ netcat/utils_test.go
@@ -37,3 +37,36 @@ func TestReplaceSpecialCharacters(t *testing.T) {
 		})
 	}
 }
+
+func TestTrimSpace(t *testing.T) {
+	tTests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "String with newline at the end",
+			input:    "Hello World\n",
+			expected: "Hello World",
+		},
+		{
+			name:     "Ordinary string",
+			input:    "Hello World\t",
+			expected: "Hello World",
+		},
+		{
+			name:     "String with a tab and an bell character",
+			input:    "Hello World\\a",
+			expected: "Hello World",
+		},
+	}
+	for _, tItem := range tTests {
+		t.Run(tItem.name, func(t *testing.T) {
+			result := trimSpace(tItem.input)
+			fmt.Printf("Result: %q\nExpected: %q\n", result, tItem.expected)
+			if result != tItem.expected {
+				t.Errorf("replaceSpecialcharacters() args = %v, want %v", result, tItem.expected)
+			}
+		})
+	}
+}
